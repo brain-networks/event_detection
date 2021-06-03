ee642a2e830cf05a30c83e1a6ab75ab5d3137a2f
vferrer <vferrer@bcbl.eu>
2021-06-03 15:05:15 +0200

    add AUCs event detection

--- ev.py
+++ ev.py
@@ -100,7 +100,7 @@ def event_detection(DATA_file,atlas,surrprefix='',sursufix='',segments=True):
     etspeaks = tspeaks[:,u]*tspeaks[:,v]
     # calculate mean co-fluctuation (edge time series) across all peaks
     mu = np.nanmean(etspeaks,0)
-    return rss, rssr, idxpeak, etspeaks, mu
+    return ets, rss, rssr, idxpeak, etspeaks, mu
 
 subject="sub-002ParkMabCm"
 nROI="200"
@@ -108,19 +108,22 @@ DIR="/bcbl/home/public/PARK_VFERRER/PFM_data"
 TEMP = "/bcbl/home/public/PARK_VFERRER/PFM_data/temp_"+subject+"_"+nROI
 DATA_file=opj(DIR, "pb06.sub-002ParkMabCm.denoised_no_censor_beta.nii.gz")
 
-rss_beta ,rssr_beta, idxpeak_beta, etspeaks_beta, mu_beta = event_detection(DATA_file,opj(TEMP,'atlas.nii.gz'),opj(TEMP,'surrogate_'),'_beta')
+ets_beta, rss_beta ,rssr_beta, idxpeak_beta, etspeaks_beta, mu_beta = event_detection(DATA_file,opj(TEMP,'atlas.nii.gz'),opj(TEMP,'surrogate_'),'_beta')
 DATA_file=opj(DIR, "pb06.sub-002ParkMabCm.denoised_no_censor.nii.gz")
-# rss_orig, idxpeak_orig, etspeaks_orig, mu_orig = event_detection(DATA_file,opj(TEMP,'atlas.nii.gz'))
-rss_orig_sur, rssr_orig_sur, idxpeak_orig_sur, etspeaks_orig_sur, mu_orig_sur = event_detection(DATA_file,opj(TEMP,'atlas.nii.gz'),opj(TEMP,'surrogate_'))
+# ets_, rss_orig, idxpeak_orig, etspeaks_orig, mu_orig = event_detection(DATA_file,opj(TEMP,'atlas.nii.gz'))
+ets_orig_sur, rss_orig_sur, rssr_orig_sur, idxpeak_orig_sur, etspeaks_orig_sur, mu_orig_sur = event_detection(DATA_file,opj(TEMP,'atlas.nii.gz'),opj(TEMP,'surrogate_'))
 DATA_file=opj(DIR, "pb06.sub-002ParkMabCm.denoised_no_censor_fitt.nii.gz")
-rss_fitt, rssr_fitt, idxpeak_fitt, etspeaks_fitt, mu_fitt = event_detection(DATA_file,opj(TEMP,'atlas.nii.gz'),opj(TEMP,'surrogate_'))
+ets_fitt, rss_fitt, rssr_fitt, idxpeak_fitt, etspeaks_fitt, mu_fitt = event_detection(DATA_file,opj(TEMP,'atlas.nii.gz'),opj(TEMP,'surrogate_'))
+DATA_file=opj(DIR, "sub-002ParkMabCm_AUC_200.nii.gz")
+ets_AUC, rss_AUC, rssr_AUC, idxpeak_AUC, etspeaks_AUC, mu_AUC = event_detection(DATA_file,opj(TEMP,'atlas.nii.gz'),opj(TEMP,'surrogate_AUC_'))
+
 # plot rss time series, null, and significant peaks
 
 # plt.legend([ph[0], qh],['null','significant','orig'])
 # plot rss time series, null, and significant peaks
 greymap = cm.get_cmap('Greys')
 colors=greymap(np.linspace(0,0.65,rssr_orig_sur.shape[1]))
-fig,axs=plt.subplots(3,1)
+fig,axs=plt.subplots(4,1)
 for i in range(rssr_orig_sur.shape[1]):
     axs[0].plot(range(rssr_orig_sur.shape[0]),rssr_orig_sur[:,i],color=colors[i])
 axs[0].plot(idxpeak_orig_sur,rss_orig_sur[idxpeak_orig_sur],'r*',label='orig_sur-peaks')
@@ -138,6 +141,12 @@ for i in range(rssr_orig_sur.shape[1]):
 axs[2].plot(idxpeak_beta,rss_beta[idxpeak_beta],'r*',label='beta-peaks')
 axs[2].plot(range(rss_beta.shape[0]),rss_beta,'k','linewidth',2,label='beta')
 axs[2].set_title('Betas')
+
+for i in range(rssr_orig_sur.shape[1]):
+    axs[3].plot(range(rssr_AUC.shape[0]),rssr_AUC[:,i],color=colors[i])
+axs[3].plot(idxpeak_AUC,rss_AUC[idxpeak_AUC],'r*',label='AUC-peaks')
+axs[3].plot(range(rss_AUC.shape[0]),rss_AUC,'k','linewidth',2,label='AUC')
+axs[3].set_title('AUCs')
 
 fig=plt.figure()
 rss_orig_norm=(rss_orig_sur-rss_orig_sur.min())/(rss_orig_sur.max()-rss_orig_sur.min())
